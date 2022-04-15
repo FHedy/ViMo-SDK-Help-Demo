ef450a07aeb091c21cc904f5f959931b2d059658
FHedy <89371189+FHedy@users.noreply.github.com>
2022-04-15 16:54:28 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ release = 'A00.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'recommonmark',
     'sphinx_markdown_tables'
 ]
 
